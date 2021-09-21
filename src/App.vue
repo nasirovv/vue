53f6e5e03d7b405661d83c61891e2afe1e988c63
Mirfayz Nosirov <nasirov0017@gmail.com>
2021-09-21 09:09:46 +0300

    test commits

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
-  <main>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <router-link to="/" class="navbar-brand" href="#">Laravel Vue Crud App - TechvBlogs</router-link>
-        <div class="collapse navbar-collapse">
-          <div class="navbar-nav">
-            <router-link exact-active-class="active" to="/" class="nav-item nav-link">Home</router-link>
-            <router-link exact-active-class="active" to="/user" class="nav-item nav-link">Category</router-link>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <div class="container mt-5">
-      <router-view></router-view>
-    </div>
-  </main>
+    Helloo
 </template>
 
 <script>
-export default {}
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
 import { createApp } from 'vue'
-import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router';
-import VueAxios from 'vue-axios';
-import axios from 'axios';
-import {routes} from './index';
+
 createApp(App).mount('#app')
-Vue.use(VueRouter);
-Vue.use(VueAxios, axios);
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: routes
-});
-
